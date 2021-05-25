199813e5a1c98143e660184f2f4cf54dbeab4ca0
Vladimir <zemetrans21@yandex.ru>
2021-05-26 02:34:53 +0300

    fix: пофиксить ошибки в jsx

--- src/App.js
+++ src/App.js
@@ -36,11 +36,11 @@ function App() {
       </footer>
 
       <div className="popup edit-form edit-form_type_profile">
-        <form name="editProfileForm" className="edit-form__form-container" novalidate>
+        <form name="editProfileForm" className="edit-form__form-container" noValidate>
           <h2 className="edit-form__form-description">Редактировать профиль</h2>
-          <input type="text" name="editProfileName" id="name-input" className="edit-form__info-input edit-form__info-input_type_name" placeholder="Имя" value="" required minlength="2" maxlength="40"/>
+          <input type="text" name="editProfileName" id="name-input" className="edit-form__info-input edit-form__info-input_type_name" placeholder="Имя" value="" required minLength="2" maxLength="40"/>
           <span className="edit-form__error-text name-input-error"></span>
-          <input type="text" name="editProfileDescription" id="description-input" className="edit-form__info-input edit-form__info-input_type_description" placeholder="Описание" value="" required minlength="2" maxlength="200"/>
+          <input type="text" name="editProfileDescription" id="description-input" className="edit-form__info-input edit-form__info-input_type_description" placeholder="Описание" value="" required minLength="2" maxLength="200"/>
           <span className="edit-form__error-text description-input-error"></span>
           <button aria-label="Сохранить" type="submit" className="edit-form__submit-button edit-form__submit-button_type_profile">Сохранить</button>
           <button aria-label="Закрыть" type="button" className="popup__close-btn edit-form__close-button edit-form__close-button_type_profile"></button>
@@ -48,9 +48,9 @@ function App() {
       </div>
 
       <div className="popup edit-form edit-form_type_place">
-        <form name="editProfileForm" className="edit-form__form-container" novalidate>
+        <form name="editProfileForm" className="edit-form__form-container" noValidate>
           <h2 className="edit-form__form-description">Новое место</h2>
-          <input type="text" name="editPlaceName" id="place-input" className="edit-form__info-input edit-form__info-input_type_place" placeholder="Название" value="" required minlength="2" maxlength="30"/>
+          <input type="text" name="editPlaceName" id="place-input" className="edit-form__info-input edit-form__info-input_type_place" placeholder="Название" value="" required minLength="2" maxLength="30"/>
           <span className="edit-form__error-text place-input-error"></span>
           <input type="url" name="editLinkPlace" id="url-input" className="edit-form__info-input edit-form__info-input_type_link" placeholder="Ссылка на картинку" value="" required/>
           <span className="edit-form__error-text url-input-error"></span>
@@ -60,7 +60,7 @@ function App() {
       </div>
 
       <div className="popup edit-form edit-form_type_delete">
-        <form name="editProfileDelete" className="edit-form__form-container" novalidate>
+        <form name="editProfileDelete" className="edit-form__form-container" noValidate>
           <h2 className="edit-form__form-description edit-form__form-description_type_delete">Вы уверены?</h2>
           <button aria-label="Удалить" type="submit" className="edit-form__submit-button edit-form__submit-button_type_delete">Да</button>
           <button aria-label="Закрыть" type="button" className="popup__close-btn edit-form__close-button edit-form__close-button_type_place"></button>
@@ -68,7 +68,7 @@ function App() {
       </div>
 
       <div className="popup edit-form edit-form_type_avatar">
-        <form name="editAvatarForm" className="edit-form__form-container" novalidate>
+        <form name="editAvatarForm" className="edit-form__form-container" noValidate>
           <h2 className="edit-form__form-description">Обновить аватар</h2>
           <input type="url" name="editLinkAvatar" id="url-input-avatar" className="edit-form__info-input edit-form__info-input_type_link" placeholder="Ссылка на аватар" value="" required/>
           <span className="edit-form__error-text url-input-avatar-error"></span>
