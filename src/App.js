eee7c867ef5c9bf9b460c711b88b653884a4a95d
Vladimir <zemetrans21@yandex.ru>
2021-05-26 04:01:57 +0300

    feat: добавить ещё три попапа через компонент

--- src/App.js
+++ src/App.js
@@ -50,38 +50,11 @@ function App() {
       <Header />
       <Main />
       <Footer />
+        
       <PopupWithForm name='profile' title='Редактировать профиль' children={editProfilePopupChildren} />
-
-
-      <div className="popup edit-form edit-form_type_place">
-        <form name="editProfileForm" className="edit-form__form-container" noValidate>
-          <h2 className="edit-form__form-description">Новое место</h2>
-          <input type="text" name="editPlaceName" id="place-input" className="edit-form__info-input edit-form__info-input_type_place" placeholder="Название" value="" required minLength="2" maxLength="30"/>
-          <span className="edit-form__error-text place-input-error"></span>
-          <input type="url" name="editLinkPlace" id="url-input" className="edit-form__info-input edit-form__info-input_type_link" placeholder="Ссылка на картинку" value="" required/>
-          <span className="edit-form__error-text url-input-error"></span>
-          <button aria-label="Сохранить" type="submit" className="edit-form__submit-button edit-form__submit-button_type_place">Создать</button>
-          <button aria-label="Закрыть" type="button" className="popup__close-btn edit-form__close-button edit-form__close-button_type_place"></button>
-        </form>
-      </div>
-
-      <div className="popup edit-form edit-form_type_delete">
-        <form name="editProfileDelete" className="edit-form__form-container" noValidate>
-          <h2 className="edit-form__form-description edit-form__form-description_type_delete">Вы уверены?</h2>
-          <button aria-label="Удалить" type="submit" className="edit-form__submit-button edit-form__submit-button_type_delete">Да</button>
-          <button aria-label="Закрыть" type="button" className="popup__close-btn edit-form__close-button edit-form__close-button_type_place"></button>
-        </form>
-      </div>
-
-      <div className="popup edit-form edit-form_type_avatar">
-        <form name="editAvatarForm" className="edit-form__form-container" noValidate>
-          <h2 className="edit-form__form-description">Обновить аватар</h2>
-          <input type="url" name="editLinkAvatar" id="url-input-avatar" className="edit-form__info-input edit-form__info-input_type_link" placeholder="Ссылка на аватар" value="" required/>
-          <span className="edit-form__error-text url-input-avatar-error"></span>
-          <button aria-label="Сохранить" type="submit" className="edit-form__submit-button edit-form__submit-button_type_place">Сохранить</button>
-          <button aria-label="Закрыть" type="button" className="popup__close-btn edit-form__close-button edit-form__close-button_type_place"></button>
-        </form>
-      </div>
+      <PopupWithForm name='place' title='Новое место' children={addPlacePopupChildren} />
+      <PopupWithForm name='delete' title='Вы уверены?' children={deleteCardPopupChildren} />
+      <PopupWithForm name='avatar' title='Обновить аватар' children={editAvatarPopupChildren} />
 
       <div className="popup image-popup">
         <div className="image-popup__container">
