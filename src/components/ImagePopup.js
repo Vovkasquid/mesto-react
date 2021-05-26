bad4c623ec569427eb6fad47ead9852dedbc3a64
Vladimir Blinov <zemetrans21@yandex.ru>
2021-05-26 20:08:35 +0300

    fix: исправить ошибки в компоненте ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,8 +2,8 @@ function ImagePopup({card, onClose}) {
   return (
     <div className={`popup image-popup ${card && `popup_status_active`}`}>
       <div className="image-popup__container">
-        <img src={card && card.link} alt="#" className="image-popup__picture"/>
-        <p className="image-popup__description">{card && card.name}</p>
+        <img src={card?.link} alt={card?.name} className="image-popup__picture"/>
+        <p className="image-popup__description">{card?.name}</p>
         <button aria-label="Закрыть" type="button" className="popup__close-btn image-popup__close-button" onClick={onClose}></button>
       </div>
     </div>
