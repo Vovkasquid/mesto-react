775f567c9afb20350e0415a7798492e08a148af6
Vladimir <zemetrans21@yandex.ru>
2021-05-26 07:11:35 +0300

    fix: исправить ворнинг

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PopupWithForm({name, title, children, isOpen, onClose}) {
   return (
-    <div className={isOpen ? `popup edit-form edit-form_type_${name} popup_status_active` : 'popup edit-form edit-form_type_${name}'}>
+    <div className={isOpen ? `popup edit-form edit-form_type_${name} popup_status_active` : `popup edit-form edit-form_type_${name}`}>
       <form name="editProfileForm" className="edit-form__form-container" noValidate>
         <h2 className="edit-form__form-description">{title}</h2>
         {children}
