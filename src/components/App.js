d1aa4a312310e34ecfb6ea1e101c186d872fb564
Vladimir Blinov <zemetrans21@yandex.ru>
2021-05-26 19:07:51 +0300

    fix: перенести App в components. Переименовать Api в api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import './index.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import PopupWithForm from './components/PopupWithForm';
-import ImagePopup from './components/ImagePopup';
+import '../index.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
 import React from 'react';
 
 function App() {
