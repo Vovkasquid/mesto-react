9a48a31bc0a8c4583152e5eb1da41a2fae4d2dde
Vladimir Blinov <zemetrans21@yandex.ru>
2021-06-21 19:16:46 +0300

    fix: сделать уникальное имя для каждой форм + убрать novalidate

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,7 +5,7 @@ function PopupWithForm({name, title, isOpen, onClose, children, buttonText, onSu
   // Который будет указан между тегами компонента в App
   return (
     <div className={`${isOpen && `popup_status_active`} popup edit-form edit-form_type_${name}`}>
-      <form name="editProfileForm" className="edit-form__form-container" noValidate onSubmit={onSubmit}>
+      <form name={name + "EditForm"} className="edit-form__form-container" onSubmit={onSubmit}>
         <h2 className="edit-form__form-description">{title}</h2>
         {children}
         <button aria-label={buttonText} type="submit" className={`edit-form__submit-button edit-form__submit-button_type_${name}`}>{buttonText}</button>
